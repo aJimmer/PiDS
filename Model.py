818531d3603cf4f53ddb03167f8b6062dfb8d94b
Angel Jimenez <ajl890889702@csu.fullerton.edu>
2020-10-17 16:37:21 -0700

    reverting to test data

--- Model.py
+++ Model.py
@@ -45,11 +45,9 @@ def main():
 
 
     #for i in range(data.shape[0]):
-    print(input_details)
-    test = np.random.random_sample(input_shape)
-    input_data = np.array(test, dtype=np.float32)
-    
-    interpreter.set_tensor(input_details[0]['index'], test)
+    print(input_details) 
+    input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
+    interpreter.set_tensor(input_details[0]['index'], input_data)
     
     print("Success!")
     #interpreter.invoke()
