3d24d776c0a2ab55fe53d8573270459b4bf2692e
Angel Jimenez <ajl890889702@csu.fullerton.edu>
2020-10-17 15:57:22 -0700

    ...

--- Model.py
+++ Model.py
@@ -52,12 +52,12 @@ def main():
 
         interpreter.set_tensor(input_details[0]['index'], input_data)
 
-        interpreter.invoke()
+        #interpreter.invoke()
 
         # The function `get_tensor()` returns a copy of the tensor data.
         # Use `tensor()` in order to get a pointer to the tensor.
-        output_data = interpreter.get_tensor(output_details[0]['index'])
-        print(output_data)
+        #output_data = interpreter.get_tensor(output_details[0]['index'])
+        #print(output_data)
 '''
     for _ in range(args.count):
         start = time.perf_counter()
