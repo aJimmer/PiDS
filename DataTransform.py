f5f35ded516ebb9a10d680fc7dbe6a119b35ad2b
Angel Jimenez <ajl890889702@csu.fullerton.edu>
2020-10-17 13:49:24 -0700

    Updated output data for ML

--- DataTransform.py
+++ DataTransform.py
@@ -34,7 +34,7 @@ def encode_numeric_zscore(df, name, mean=None, sd=None):
     df[name] = (df[name] - mean) / sd
 
 log_to_df = LogToDataFrame()
-conn_log_df = log_to_df.create_dataframe('/home/mendel/conn.log')
+conn_log_df = log_to_df.create_dataframe('/mnt/thesis/captures/nms/logs/conn.log')
 
 conn_log_df['key'] = list(zip(conn_log_df["id.orig_h"], conn_log_df["id.orig_p"], conn_log_df["id.resp_h"], conn_log_df["id.resp_p"]))
 conn_log_df['orig_bytes'] = conn_log_df['orig_bytes'].fillna(0)
@@ -75,4 +75,5 @@ final_df['label'] = 1
 Y_normal = final_df['label']
 X_normal = final_df.drop(["x", "y","proto", "service", "local_orig", "local_resp", "history", "id.orig_h", "id.resp_h","uid", "key", "tunnel_parents", "conn_state", "label"], axis = 1)
 
+X_normal.to_csv('/mnt/thesis/captures/nms/data/data.csv')
 
