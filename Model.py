c82d2f96a66017acf7edce9c945f73076cd9ea9e
Angel Jimenez <ajl890889702@csu.fullerton.edu>
2020-10-17 17:31:22 -0700

    ...

--- Model.py
+++ Model.py
@@ -55,7 +55,7 @@ def main():
         interpreter.invoke()
         
         inference_time = time.perf_counter() - start
-        output_data = interpreter.get_output_details()
+        output_data = interpreter.get_output_details()[0]['index']
         print(output_data, '%.2f ms' % (inference_time * 1000))
         
 '''
